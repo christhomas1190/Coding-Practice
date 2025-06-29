512668e2042f9a93c168641a66b16ec3fd042efd
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-06-29 17:20:44 -0400

    methods written

--- src/twosumsolution/twosumsolution.java
+++ src/twosumsolution/twosumsolution.java
@@ -1,4 +1,17 @@
 package twosumsolution;
 
 public class twosumsolution {
+
+    public int[] solution(int[] nums, int target){
+        if(nums.length==0) return null;
+
+        for(int i = 0; i < nums.length;i++){
+            for (int j = i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[] {i,j};
+                }
+            }
+        }
+        return null;
+    }
 }
