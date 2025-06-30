a8230656d7aa60e08509a694a742513ef365c0f0
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-06-30 16:46:00 -0400

    null test written

--- tests/mergesortedlinkedlisttest.java
+++ tests/mergesortedlinkedlisttest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class mergesortedlinkedlisttest {
 
@@ -33,6 +34,19 @@ public class mergesortedlinkedlisttest {
 
         // And: The length should match the expected array
         assertEquals(expected.length, i);
+    }
+    @Test
+    void oneEmptyListNode(){
+        // Given: Two sorted linked lists
+
+        ListNode list1 = null;
+        ListNode list2 = null;
+
+        // When: Merging the two lists
+
+        ListNode results = sortedMergeSolution.mergeTwoLists(list1,list1);
+        // Then: The result should be a sorted list containing all elements
+        assertNull(results);
     }
 
 }
