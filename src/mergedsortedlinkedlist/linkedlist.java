6d80b6a2bdda27b4717c62c1978bbc0c91d85776
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-06-30 16:09:25 -0400

    checkpoint

--- src/mergedsortedlinkedlist/linkedlist.java
+++
@@ -1,4 +0,0 @@
-package mergedsortedlinkedlist;
-
-public class linkedlist {
-}
--- src/mergedsortedlinkedlist/sortedmergesolution.java
+++ src/mergedsortedlinkedlist/sortedmergesolution.java
@@ -1,4 +1,15 @@
 package mergedsortedlinkedlist;
 
 public class sortedmergesolution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode();
+        ListNode current = dummy;
+
+        ListNode p1 = list1;
+        ListNode p2 = list2;
+
+
+        return dummy.next;
+    }
 }
+
