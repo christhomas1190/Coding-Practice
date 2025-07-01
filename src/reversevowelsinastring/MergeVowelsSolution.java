70a5aeface7840a9a0a4135b5635d6447d2849f1
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-07-01 17:37:53 -0400

    solution started

--- src/reversevowelsinastring/MergeVowelsSolution.java
+++ src/reversevowelsinastring/MergeVowelsSolution.java
@@ -2,6 +2,9 @@ package reversevowelsinastring;
 
 public class MergeVowelsSolution {
     public String reverseVowels(String s){
-        Str
+        String vowels = "AEIOUaeiou";
+        StringBuilder sb= new StringBuilder(s);
+        int leftMoveRight = 0;
+        int rightMoveLeft = s.length()-1;
     }
 }
