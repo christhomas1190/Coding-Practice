01e6fdb211572b309dacf463f6b9d6fafadeed25
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-07-01 17:53:27 -0400

    code finished tests to come

--- src/reversevowelsinastring/MergeVowelsSolution.java
+++ src/reversevowelsinastring/MergeVowelsSolution.java
@@ -11,6 +11,16 @@ public class MergeVowelsSolution {
             while(leftMoveRight<rightMoveLeft&& !vowels.contains(sb.deleteCharAt(leftMoveRight))){
                 leftMoveRight++;
             }
+            while(leftMoveRight<rightMoveLeft&& !vowels.contains(sb.deleteCharAt(rightMoveLeft))) {
+                rightMoveLeft++;
+            }
+            char temp = sb.charAt(leftMoveRight);
+            sb.setCharAt(leftMoveRight, sb.charAt(rightMoveLeft));
+            sb.setCharAt(rightMoveLeft,temp);
+            leftMoveRight++;
+            rightMoveLeft--;
         }
+            return sb.toString();
     }
+
 }
