9feec52a5bb6ff3d71dadfbfb9b6919fd14154fd
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-07-02 08:07:24 -0400

    before each setup

--- tests/reversevowelsinastringtest.java
+++ tests/reversevowelsinastringtest.java
@@ -1,2 +1,14 @@
+import org.junit.jupiter.api.BeforeEach;
+import reversevowelsinastring.MergeVowelsSolution;
+
 public class reversevowelsinastringtest {
+
+    private MergeVowelsSolution mergeVowelsSolution;
+
+    @BeforeEach
+    void setup(){
+        mergeVowelsSolution = new MergeVowelsSolution();
+    }
+
+
 }
