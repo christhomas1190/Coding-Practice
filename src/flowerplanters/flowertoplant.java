191b5e6b49b7323ba9bc674401011a00d410283f
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-07-04 10:46:41 -0400

    code written

--- src/flowerplanters/flowertoplant.java
+++ src/flowerplanters/flowertoplant.java
@@ -4,7 +4,17 @@ public class flowertoplant {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
     if(flowerbed.length==0) return false;
     int flowersToPlant = n;
-
-}
-
+        for(int i = 0; i<flowerbed.length;i++){
+            if (flowerbed[i] == 0 &&
+                    (i == 0 || flowerbed[i - 1] == 0) &&
+                    (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
+                flowerbed[i] = 1;
+                flowersToPlant--;
+                if (flowersToPlant == 0) {
+                    return true;
+                }
+            }
+        }
+        return flowersToPlant<=0;
+    }
 }
