9f7650031aa56e4ee3c8a494858c9693682afe55
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-07-04 10:49:33 -0400

    Test class created

--- src/flowerplanters/FlowerToPlant.java
+++ src/flowerplanters/FlowerToPlant.java
@@ -1,6 +1,6 @@
 package flowerplanters;
 
-public class flowertoplant {
+public class FlowerToPlant {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
     if(flowerbed.length==0) return false;
     int flowersToPlant = n;
---
+++ tests/flowerplatertest.java
@@ -0,0 +1,15 @@
+import flowerplanters.FlowerToPlant;
+import org.junit.jupiter.api.BeforeEach;
+
+public class flowerplatertest {
+
+    FlowerToPlant flowerToPlant;
+
+    @BeforeEach
+    void setup(){
+        flowerToPlant=new FlowerToPlant();
+    }
+
+
+
+}
