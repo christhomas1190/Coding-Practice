ce75586938cde31e7f6affb7d668f063396f74ee
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-07-04 10:45:31 -0400

    methos created

--- src/flowerplanters/flowertoplant.java
+++ src/flowerplanters/flowertoplant.java
@@ -1,5 +1,10 @@
 package flowerplanters;
 
 public class flowertoplant {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+    if(flowerbed.length==0) return false;
+    int flowersToPlant = n;
+
+}
 
 }
