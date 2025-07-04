98482f565a857d0b2d679d9002114851f457548b
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-07-04 10:57:39 -0400

    First test passed

--- tests/flowerplatertest.java
+++ tests/flowerplatertest.java
@@ -1,15 +1,32 @@
 import flowerplanters.FlowerToPlant;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertTrue;
 
 public class flowerplatertest {
 
     FlowerToPlant flowerToPlant;
-
+    int[] planter;
+    int toPlant;
     @BeforeEach
     void setup(){
         flowerToPlant=new FlowerToPlant();
+
     }
 
+    @Test
+    void arrayof5(){
+        //Given
+        int[] planter = new int[]{1, 0, 0, 0, 1};
+        toPlant=1;
+
+        //When
+        boolean results = flowerToPlant.canPlaceFlowers(planter, toPlant);
+
+        //Then
+        assertTrue(results);
+    }
 
 
 }
