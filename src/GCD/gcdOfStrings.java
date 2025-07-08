0e86bd9538842a1c0c7ecd1784163944fae99683
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-07-08 16:50:23 -0400

    checkpoint

--- src/GCD/gcdOfStrings.java
+++ src/GCD/gcdOfStrings.java
@@ -1,9 +1,20 @@
 package GCD;
 
-public class gcdOfStrings {
     public String gcdOfStrings(String str1, String str2) {
-        if (!(str1 + str2).equals(str2 + str1)) {
-            return "";
-        }
+        int minLength = Math.min(str1.length(), str2.length());
+        String result = "";
 
+        for (int i = 1; i <= minLength; i++) {
+            if (str1.length() % i == 0 && str2.length() % i == 0) {
+                String candidate = str1.substring(0, i);
+                boolean isValidForStr1 = true;
+                for (int j = 0; j < str1.length(); j += candidate.length()) {
+                    if (!str1.substring(j, j + candidate.length()).equals(candidate)) {
+                        isValidForStr1 = false;
+                        break;
+                    }
+                }
+            }
+        }
     }
+
