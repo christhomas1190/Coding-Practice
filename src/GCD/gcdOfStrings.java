f428c9a3b5c374d700e54fb6bba3bcde17a2305c
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-07-08 16:48:13 -0400

    first check written

---
+++ src/GCD/gcdOfStrings.java
@@ -0,0 +1,9 @@
+package GCD;
+
+public class gcdOfStrings {
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+
+    }
