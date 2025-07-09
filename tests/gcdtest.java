3c2c6d61c936b082c520eedb81db61592f4123ba
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-07-09 08:37:10 -0400

    ABC test passed

--- tests/gcdtest.java
+++ tests/gcdtest.java
@@ -1,7 +1,28 @@
 import GCD.GreatestCommonDenom;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class gcdtest {
 
     private GreatestCommonDenom greatestCommonDenom;
+
+    @BeforeEach
+    void setUp (){
+        greatestCommonDenom = new GreatestCommonDenom();
+    }
+
+    @Test
+    void expectABC(){
+        //given
+        String str1 = "ABCABC", str2 = "ABC";
+        //when
+        String output = greatestCommonDenom.gcdOfStrings(str1,str2);
+        //then
+        assertEquals("ABC",output);
+
+    }
 
 }
