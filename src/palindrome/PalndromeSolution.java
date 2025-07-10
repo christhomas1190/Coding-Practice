2cb43d8411f6833328a18c415102d973c6b13711
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-07-10 12:06:22 -0400

    Palindrome started

---
+++ src/palindrome/PalndromeSolution.java
@@ -0,0 +1,10 @@
+package palindrome;
+
+public class PalndromeSolution {
+
+    public boolean isPalindrome(int x) {
+        // Negative numbers are never palindromes
+        if (x < 0) {
+            return false;
+        }
+}
