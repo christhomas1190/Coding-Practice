db990184cd34e087d8f3e4c67112b4060a66bfa8
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-07-10 12:06:55 -0400

    Checkpoint

--- src/palindrome/PalndromeSolution.java
+++ src/palindrome/PalndromeSolution.java
@@ -7,4 +7,12 @@ public class PalndromeSolution {
         if (x < 0) {
             return false;
         }
+        // Convert number to string
+        String numStr = String.valueOf(x);
+
+        // Use two pointers to compare characters from both ends
+        int left = 0;
+        int right = numStr.length() - 1;
+
+
 }
