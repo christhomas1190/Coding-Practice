7592127cf5ff5f0f4fa122d832120c3600e42319
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-07-11 09:44:21 -0400

    test setup

---
+++ tests/Palindrometest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.BeforeEach;
+import palindrome.PalndromeSolution;
+
+public class Palindrometest {
+
+    PalndromeSolution palindromeSolution;
+
+    @BeforeEach
+void setUp(){
+        palindromeSolution = new PalndromeSolution();
+    }
+}
