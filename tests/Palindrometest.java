ca8e28937c2548ad528ced63749af01f1db438ee
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-07-11 10:04:05 -0400

    Negative digit test

--- tests/Palindrometest.java
+++ tests/Palindrometest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import palindrome.PalndromeSolution;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class Palindrometest {
@@ -29,5 +30,15 @@ void typicalTest(){
         boolean result = palindromeSolution.isPalindrome(palindrome);
         //then
         assertTrue(result);
+    }
+
+@Test
+    void negativeNumberTest(){
+        //given
+        int palindrome = -12121;
+        //when
+        boolean result = palindromeSolution.isPalindrome(palindrome);
+        //then
+        assertFalse(result);
     }
 }
