f9814dfa6d34cefee9f0aab0b14f2158db52e4b8
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-07-14 18:13:39 -0400

    comments begun

--- src/highestaltitude/HighestAltitude.java
+++ src/highestaltitude/HighestAltitude.java
@@ -6,6 +6,7 @@ public class HighestAltitude {
         int highestNet = 0;
 
             for(int i=0; i< gain.length; i++){
+                //Adds values in array, and stores them in temp
                 temp += gain[i];
                 if(temp>highestNet){
                     highestNet=temp;
