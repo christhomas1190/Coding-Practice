8a7220458616985cd07c603206a2d8c7042d2d57
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-07-14 18:09:45 -0400

    Method begun

---
+++ src/highestaltitude/HighestAltitude.java
@@ -0,0 +1,13 @@
+package highestaltitude;
+
+public class HighestAltitude {
+    public int findHighestAltitude(int[] gain){
+        int temp =0;
+        int highestNet = 0;
+
+            for(int i=0; i< gain.length; i++){
+                temp += gain[i];
+                if(temp>highestNet)
+            }
+    }
+}
