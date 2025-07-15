6c546f5b6dc416159a99dec537db6dd2b5560630
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-07-15 08:33:40 -0400

    all positive test passed

--- tests/highestaltitudetest.java
+++ tests/highestaltitudetest.java
@@ -1,8 +1,25 @@
 import highestaltitude.HighestAltitude;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class highestaltitudetest {
 
     HighestAltitude highestAltitude;
 
+@BeforeEach
+    void setUp(){
+    highestAltitude = new HighestAltitude();
+}
 
+@Test
+    void allPositiveTest (){
+    //Given
+    int[] gain = {3, 5, 2, 4, 6};
+    //When
+    int output = 20;
+    //then
+    assertEquals(output,highestAltitude.findHighestAltitude(gain));
+}
 }
