0d363ed4f399c9fee851391601ca87c330d6ec51
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-07-19 12:34:46 -0400

    difference of two arrays begun

--- src/differenceoftwoarrays/DifferenceOfTwoArrays.java
+++ src/differenceoftwoarrays/DifferenceOfTwoArrays.java
@@ -1,4 +1,10 @@
 package differenceoftwoarrays;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DifferenceOfTwoArrays {
+    List<List<Integer>> temp1 = new ArrayList<>();
+    List<List<Integer>> temp2 = new ArrayList<>();
+    List<List<Integer>> result = new ArrayList<>();
 }
