d0bf9c8e993b7ff2fccf139d58e269bf8b102481
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-07-19 12:41:44 -0400

    first loop done

--- src/differenceoftwoarrays/DifferenceOfTwoArrays.java
+++ src/differenceoftwoarrays/DifferenceOfTwoArrays.java
@@ -4,7 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DifferenceOfTwoArrays {
-    List<List<Integer>> temp1 = new ArrayList<>();
-    List<List<Integer>> temp2 = new ArrayList<>();
-    List<List<Integer>> result = new ArrayList<>();
+    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+
+        List<Integer> temp1 = new ArrayList<>();
+        List<Integer> temp2 = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
+
+        for(int i = 0; i <nums1.length; i++){
+            int temp = nums1[i];
+            boolean found = false;
+           temp1.add(temp)
+            }
+        }
+
+        return results;
+    }
 }
