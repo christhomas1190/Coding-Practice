77e4377fdbf04b8b01174fcdd4659d6c185ae5e2
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-07-20 15:12:17 -0400

    Duplicate numbers test passed

--- tests/differenceoftwoarraystest.java
+++ tests/differenceoftwoarraystest.java
@@ -54,4 +54,16 @@ public class differenceoftwoarraystest {
         assertEquals(Arrays.asList(), result.get(0));
         assertEquals(Arrays.asList(1,2,3), result.get(1));
     }
+    @Test
+    void DuplicateNumbersArrays(){
+        //Given
+        nums1 = new int[]{1, 2, 2, 3};
+        nums2 = new int[]{2, 3, 4, 4};
+        //When
+        List<List<Integer>>result=differenceOfTwoArrays.findDifference(nums1,nums2);
+        //Then
+        assertEquals(Arrays.asList(1), result.get(0));
+        assertEquals(Arrays.asList(4), result.get(1));
+    }
+
 }
