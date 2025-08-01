4443641273338abba7aa0da57996e708e7a2fcb6
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-08-01 11:28:49 -0400

    find combos created to be called recursively

--- src/CombinationSum.java
+++ src/CombinationSum.java
@@ -1,2 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class CombinationSum {
+
+    public List<List<Integer>> combinationSumIII(int k, int n){
+        List<List<Integer>> result=new ArrayList<>();
+        findCombos(k,n,1,new ArrayList<>(),result);
+        return result;
+    }
+
 }
