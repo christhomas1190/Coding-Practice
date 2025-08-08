9d28577deebad0ffe93e1e511fac1654ae8e7450
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-08-08 14:50:59 -0400

    weddings finished back to work

--- src/combinationsum/CombinationSum.java
+++ src/combinationsum/CombinationSum.java
@@ -1,3 +1,5 @@
+package combinationsum;
+
 import java.util.ArrayList;
 import java.util.List;
 
---
+++ tests/combinationsumtest.java
@@ -0,0 +1,18 @@
+import combinationsum.CombinationSum;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.List;
+
+public class combinationsumtest {
+
+    private CombinationSum combinationSum;
+
+    @BeforeEach
+    void setUp(){
+        int k;
+        int n;
+        List<Integer> current;
+        List<List<Integer>> result;
+    }
+
+}
