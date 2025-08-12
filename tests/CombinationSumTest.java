e1f33e90f2badcdee7567dd7135af20724317fd8
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-08-12 09:22:14 -0400

    tests incoming

--- tests/CombinationSumTest.java
+++ tests/CombinationSumTest.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CombinationSumTest {
@@ -30,5 +31,34 @@ public class CombinationSumTest {
 
         // Then
         assertEquals(expected, actual);
+    }
+    @Test
+    void fiveNumSum() {
+        // Given
+        int k = 9;
+        int n = 45;
+        List<List<Integer>> expected = new ArrayList<>();
+        expected.add(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+
+        // When
+        List<List<Integer>> actual = combinationSum.combinationSumIII(k, n);
+
+        // Then
+        assertEquals(expected, actual);
+    }
+    @Test
+    void noNumSum() {
+        // Given
+        int k = 3;
+        int n = 1;
+        List<List<Integer>> expected = new ArrayList<>();
+        expected.add(Arrays.asList());
+
+        // When
+        List<List<Integer>> actual = combinationSum.combinationSumIII(k, n);
+
+        // Then
+        assertTrue(actual.isEmpty());
+
     }
 }
