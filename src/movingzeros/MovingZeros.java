4a2828562d641011453ccb509b20bbb821da7851
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-08-22 11:58:56 -0400

    first look created it will check in the number is 0, moves non zero number forward

--- src/movingzeros/MovingZeros.java
+++ src/movingzeros/MovingZeros.java
@@ -1,4 +1,15 @@
 package movingzeros;
 
 public class MovingZeros {
+    public void moveZeroes(int[] nums) {
+
+        int pos = 0;
+        for(int i = 0; i<nums.length; i++){
+            if(nums[i]!=0){
+                nums[pos]=nums[i];
+                pos++;
+            }
+        }
+
+    }
 }
