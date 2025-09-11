34365b23683566d7513526c1bf3dbebc39664bba
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-09-11 18:43:12 -0400

    loops created sub sequence created by adding char at j

--- src/subsequence/SubSequence.java
+++ src/subsequence/SubSequence.java
@@ -5,6 +5,19 @@ public class SubSequence {
         boolean result = false;
         String subSequence = "";
         int start = 0;
+        for(int i = start; i < s.length();i++){
+            for(int j = start; j< t.length(); j++){
+                if(s.charAt(i)==t.charAt(j)){
+                    subSequence += t.charAt(j);
+                    start=j+1;
+                    break;
+                }
+            }
+        }
+        if (subSequence.equals(s)) {
+            result = true;
+        }
+        return result;
     }
 
 }
